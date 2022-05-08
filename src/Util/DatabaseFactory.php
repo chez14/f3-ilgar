faf6382a58148ef59fac9d80b4c447b560e0abdf
Chris "chez14" Qiang <chris@christianto.net>
2022-05-08 15:41:41 +0700

    fix: Typing fix for DB Factory

--- src/Util/DatabaseFactory.php
+++ src/Util/DatabaseFactory.php
@@ -10,17 +10,21 @@ class DatabaseFactory
     /**
      * Create DB Util from the DB thingy, with Runner context.
      *
-     * @param mixed $db The DB
+     * @param \mixed $db The DB
      * @param Runner $runner the Runner context
      * @return DatabaseUtilInterface
      */
-    public static function createFrom(mixed $db, Runner $runner): DatabaseUtilInterface
+    public static function createFrom($db, Runner $runner): DatabaseUtilInterface
     {
         $returnPath = null;
         if ($db instanceof \DB\SQL) {
             $returnPath = new DatabaseSQLish($db, $runner);
         } elseif ($db instanceof \DB\Mongo) {
             $returnPath = new DatabaseMongoish($db, $runner);
+        } elseif (!$db) {
+            throw new InvalidArgumentException(
+                sprintf("DB variable is null.")
+            );
         } else {
             throw new InvalidArgumentException(
                 sprintf("DB Class %d is not supported.", get_class($db))
