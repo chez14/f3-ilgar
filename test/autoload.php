14bb8253c268db0359adb4bc7f03d7075a325771
Chris "chez14" Qiang <chris@christianto.net>
2022-05-08 17:16:28 +0700

    test: Don't try to load .env file when it doesn't exists

--- test/autoload.php
+++ test/autoload.php
@@ -2,5 +2,10 @@
 
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-$dotenv->load();
+// Load .env file for local development test if it exists. If its not then don't
+// run 😅
+$envPath = __DIR__ . "/../";
+if (is_file($envPath . ".env")) {
+    $dotenv = Dotenv\Dotenv::createImmutable($envPath);
+    $dotenv->load();
+}
