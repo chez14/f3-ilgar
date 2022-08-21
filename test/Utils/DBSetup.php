928fbac677218b8124bc44c563edce3bc267e02e
Chris "chez14" Qiang <chris@christianto.net>
2022-08-21 08:21:34 +0700

    test: DB setup to fix the environment problem first

--- test/Utils/DBSetup.php
+++ test/Utils/DBSetup.php
@@ -2,10 +2,21 @@
 
 namespace CHEZ14\Ilgar\Test\Utils;
 
+use Base;
 use F3;
 
 class DBSetup
 {
+
+    public static function fixEnvironment()
+    {
+        $envA = getenv();
+        $envB = Base::instance()->get('ENV');
+
+        $env = array_merge($envA, $envB);
+        Base::instance()->set('ENV', $env);
+    }
+
     /**
      * Setup the database so we can just call it on the test bench.
      *
@@ -13,18 +24,19 @@ class DBSetup
      */
     public static function setup()
     {
+        self::fixEnvironment();
         // check for DB.
         $db = null;
-        if (F3::instance()->get('ENV.db', getenv('db')) == "sqllike") {
+        if (F3::instance()->get('ENV.db') == "sqllike") {
             $db = new \DB\SQL(
-                F3::instance()->get('ENV.db_dsn', getenv('db_dsn')),
-                F3::instance()->get('ENV.db_username', getenv('db_username')) ?? "",
-                F3::instance()->get('ENV.db_password', getenv('db_password')) ?? ""
+                F3::instance()->get('ENV.db_dsn'),
+                F3::instance()->get('ENV.db_username') ?? "",
+                F3::instance()->get('ENV.db_password') ?? ""
             );
-        } elseif (F3::instance()->get('ENV.db', getenv('db')) == "mongo") {
+        } elseif (F3::instance()->get('ENV.db') == "mongo") {
             $db = new \DB\Mongo(
-                F3::instance()->get('ENV.db_dsn', getenv('db_dsn')),
-                F3::instance()->get('ENV.db_table', getenv('db_table'))
+                F3::instance()->get('ENV.db_dsn'),
+                F3::instance()->get('ENV.db_table')
             );
         }
 
