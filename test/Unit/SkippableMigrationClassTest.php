cd8cda7702645aac480670580c002f03f08d586b
Chris "chez14" Qiang <chris@christianto.net>
2022-08-21 08:05:19 +0700

    test: Add name for skippable migration class test

--- test/Unit/SkippableMigrationClassTest.php
+++ test/Unit/SkippableMigrationClassTest.php
@@ -56,7 +56,7 @@ class SkippableMigrationClassTest extends TestCase
     public function migrationTestCaseDataProvider()
     {
         return [
-            [ // (#0) checks for empty migration
+            'number: Checks for empty migration' => [
                 "existing" => [],
                 "migratable" => [
                     ['version' => '1',],
@@ -68,7 +68,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '2',],
                     ['version' => '3',],
                 ]
-            ], [ // (#1) checks for some migration that has run previously
+            ],
+            'number: Checks for some migration that has run previously' => [
                 "existing" => [
                     ['version' => '1',],
                     ['version' => '2',],
@@ -79,7 +80,8 @@ class SkippableMigrationClassTest extends TestCase
                 'expectation' => [
                     ['version' => '3',],
                 ]
-            ], [ // (#2) Further check for migration that has run previously.
+            ],
+            'number: Further check for migration that has run previously' => [
                 "existing" => [
                     ['version' => '1',],
                 ],
@@ -91,7 +93,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '2',],
                     ['version' => '3',],
                 ]
-            ], [ // (#3) Check for new migration that have version in the middle of current migration
+            ],
+            "number: Check for new migration that have version in the middle of current migration" => [
                 "existing" => [
                     ['version' => '1',],
                     ['version' => '3',],
@@ -104,7 +107,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '2',],
                     ['version' => '4',],
                 ]
-            ], [ // (#4) Check for new migration that have version in the middle of current migration
+            ],
+            "number: Check for new migration that have version in the middle of current migration" => [
                 "existing" => [
                     ['version' => '1',],
                     ['version' => '2',],
@@ -113,7 +117,8 @@ class SkippableMigrationClassTest extends TestCase
                 ],
                 "migratable" => [],
                 'expectation' => []
-            ], [ // (#5) Migration should not pick things that has been run.
+            ],
+            "number: Migration should not pick things that has been run" => [
                 "existing" => [
                     ['version' => '1',],
                     ['version' => '2',],
@@ -127,7 +132,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '4',],
                 ],
                 'expectation' => []
-            ], [ // (#6) Migration should pick things that has not been run in the list of ran migrations.
+            ],
+            "number: Migration should pick things that has not been run in the list of ran migrations" => [
                 "existing" => [
                     ['version' => '1',],
                     ['version' => '2',],
@@ -145,7 +151,7 @@ class SkippableMigrationClassTest extends TestCase
             ],
 
             // date version (date taken from E7 AO Episode Airing.)
-            [ // (#7) Checks for empty.
+            "date: Checks for empty" => [
                 "existing" => [],
                 "migratable" => [
                     ['version' => '20120413131008',],
@@ -171,7 +177,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '20120608150526',],
                     ['version' => '20120622150526',],
                 ]
-            ], [ // (#8) Checks for migration that has been ran previously.
+            ],
+            "date: Checks for migration that has been ran previously" => [
                 "existing" => [
                     ['version' => '20120413131008',],
                     ['version' => '20120420131015',],
@@ -193,7 +200,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '20120608150526',],
                     ['version' => '20120622150526',],
                 ]
-            ], [ // (#9) Checks for something in the middle
+            ],
+            "date: Checks for something in the middle" => [
                 "existing" => [
                     ['version' => '20120413131008',],
                     ['version' => '20120420131015',],
@@ -214,7 +222,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '20120608150526',],
                     ['version' => '20120622150526',],
                 ]
-            ], [ // (#10) Checks for no migratable thingy.
+            ],
+            "date: Checks for no migratable thingy." => [
                 "existing" => [
                     ['version' => '20120413131008',],
                     ['version' => '20120420131015',],
@@ -229,7 +238,8 @@ class SkippableMigrationClassTest extends TestCase
                 ],
                 "migratable" => [],
                 'expectation' => []
-            ], [ // (#11) Should not migrate existing migration
+            ],
+            "date: Should not migrate existing migration" => [
                 "existing" => [
                     ['version' => '20120413131008',],
                     ['version' => '20120420131015',],
@@ -255,7 +265,8 @@ class SkippableMigrationClassTest extends TestCase
                     ['version' => '20120622150526',],
                 ],
                 'expectation' => []
-            ], [ // (#12) Should not migrate existing migration, but will migrate one that are not there.
+            ],
+            "date: Should not migrate existing migration, but will migrate one that are not there" => [
                 "existing" => [
                     ['version' => '20120413131008',],
                     ['version' => '20120420131015',],
